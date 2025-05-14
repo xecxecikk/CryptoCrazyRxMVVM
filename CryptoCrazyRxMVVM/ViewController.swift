f432c5ca03227b0ea790e2ac51c0cd7109912dce
xecxecikk <xecekrdnz_94@outlook.com>
2025-05-14 19:59:41 +0300

    Initial Commit

---
+++ CryptoCrazyRxMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoCrazyRxMVVM
+//
+//  Created by XECE on 14.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
